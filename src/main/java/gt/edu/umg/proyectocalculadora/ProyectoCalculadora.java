3613165aa2c4cd41bd152de58b10a863af779fa9
hugog <hugog@Hugo-Vasquez>
2024-05-08 16:36:42 -0600

    prueba1usonetbenasygit111111

--- src/main/java/gt/edu/umg/proyectocalculadora/ProyectoCalculadora.java
+++ src/main/java/gt/edu/umg/proyectocalculadora/ProyectoCalculadora.java
@@ -11,6 +11,6 @@ package gt.edu.umg.proyectocalculadora;
 public class ProyectoCalculadora {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("holaaaaaaaaaaa a todoss!");
     }
 }
